222c5cfeaed1f9ba0f07f10419225d45885d7065
martin-bobek <martinbobek8@gmail.com>
2019-10-28 18:58:39 -0600

    Shifted declaration of Iterator into source file.
    Iterator is only needed by the internals of the syntax tree.

--- RegexSyntaxTree.cpp
+++ RegexSyntaxTree.cpp
@@ -6,6 +6,24 @@
 using std::vector;
 
 
+class Iterator
+{
+public:
+    Iterator(const std::string::const_iterator &it_) : it(it_) {}
+    Iterator(const Iterator &it) = default;
+    ~Iterator() = default;
+    Iterator &operator=(const Iterator &rhs) = default;
+    Iterator &operator++();
+    Iterator operator++(int);
+    bool operator==(const Iterator &rhs) const { return it == rhs.it; }
+    bool operator!=(const Iterator &rhs) const { return it != rhs.it; }
+    char operator*() const;
+    bool IsChar() const;
+    char C() const;
+private:
+    std::string::const_iterator it;
+};
+
 class Terminal : public Node
 {
 public:
--- RegexSyntaxTree.h
+++ RegexSyntaxTree.h
@@ -10,24 +10,6 @@ class Node;
 typedef std::unique_ptr<Node> pNode;
 
 
-class Iterator
-{
-public:
-    Iterator(const std::string::const_iterator &it_) : it(it_) {}
-    Iterator(const Iterator &it) = default;
-    ~Iterator() = default;
-    Iterator &operator=(const Iterator &rhs) = default;
-    Iterator &operator++();
-    Iterator operator++(int);
-    bool operator==(const Iterator &rhs) const { return it == rhs.it; }
-    bool operator!=(const Iterator &rhs) const { return it != rhs.it; }
-    char operator*() const;
-    bool IsChar() const;
-    char C() const;
-private:
-    std::string::const_iterator it;
-};
-
 class Node
 {
 public:
