445a3646bb16cde75261c41db9c933eaba3b6fc6
martin-bobek <martinbobek8@gmail.com>
2019-10-28 18:54:53 -0600

    Placed derived class declarations in source file.
    Declarations of classes deriving from Node are not needed by users of
    Node. These can be left inside the source file to avoid cluttering the
    namespace.

--- RegexSyntaxTree.cpp
+++ RegexSyntaxTree.cpp
@@ -1,6 +1,68 @@
 #include "RegexSyntaxTree.h"
 
 #include <utility>
+#include <vector>
+
+using std::vector;
+
+
+class Terminal : public Node
+{
+public:
+    Terminal(char symbol_) : symbol(symbol_) {}
+    NFA GenNfa(NFA &&nfa = NFA()) const { return NFA(symbol); }
+private:
+    const char symbol;
+};
+class NonTerminal : public Node
+{
+protected:
+    vector<pNode> nodes;
+};
+
+class Q : public NonTerminal
+{
+public:
+    Q(Iterator &it, Iterator end);
+    NFA GenNfa(NFA &&nfa = NFA()) const;
+};
+class R : public NonTerminal
+{
+public:
+    R(Iterator &it, Iterator end);
+    NFA GenNfa(NFA &&nfa = NFA()) const;
+};
+class S : public NonTerminal
+{
+public:
+    S(Iterator &it, Iterator end);
+    NFA GenNfa(NFA &&nfa = NFA()) const;
+};
+class T : public NonTerminal
+{
+public:
+    T(Iterator &it, Iterator end);
+    NFA GenNfa(NFA &&nfa = NFA()) const;
+};
+class U : public NonTerminal
+{
+public:
+    U(Iterator &it, Iterator end);
+    NFA GenNfa(NFA &&nfa = NFA()) const;
+};
+class V : public NonTerminal
+{
+public:
+    V(Iterator &it, Iterator end);
+    NFA GenNfa(NFA &&nfa = NFA()) const;
+};
+class W : public NonTerminal
+{
+public:
+    W(Iterator &it, Iterator end);
+    NFA GenNfa(NFA &&nfa = NFA()) const;
+};
+
 
 Iterator &Iterator::operator++()
 {
--- RegexSyntaxTree.h
+++ RegexSyntaxTree.h
@@ -5,9 +5,6 @@
 
 #include <memory>
 #include <string>
-#include <vector>
-
-using std::vector;
 
 class Node;
 typedef std::unique_ptr<Node> pNode;
@@ -45,63 +42,6 @@ public:
     operator bool() const { return (bool)node; }
 private:
     pNode node;
-};
-
-class Terminal : public Node
-{
-public:
-    Terminal(char symbol_) : symbol(symbol_) {}
-    NFA GenNfa(NFA &&nfa = NFA()) const { return NFA(symbol); }
-private:
-    const char symbol;
-};
-class NonTerminal : public Node
-{
-protected:
-    vector<pNode> nodes;
-};
-
-class Q : public NonTerminal
-{
-public:
-    Q(Iterator &it, Iterator end);
-    NFA GenNfa(NFA &&nfa = NFA()) const;
-};
-class R : public NonTerminal
-{
-public:
-    R(Iterator &it, Iterator end);
-    NFA GenNfa(NFA &&nfa = NFA()) const;
-};
-class S : public NonTerminal
-{
-public:
-    S(Iterator &it, Iterator end);
-    NFA GenNfa(NFA &&nfa = NFA()) const;
-};
-class T : public NonTerminal
-{
-public:
-    T(Iterator &it, Iterator end);
-    NFA GenNfa(NFA &&nfa = NFA()) const;
-};
-class U : public NonTerminal
-{
-public:
-    U(Iterator &it, Iterator end);
-    NFA GenNfa(NFA &&nfa = NFA()) const;
-};
-class V : public NonTerminal
-{
-public:
-    V(Iterator &it, Iterator end);
-    NFA GenNfa(NFA &&nfa = NFA()) const;
-};
-class W : public NonTerminal
-{
-public:
-    W(Iterator &it, Iterator end);
-    NFA GenNfa(NFA &&nfa = NFA()) const;
 };
 
 #endif
