0f33d32806f8efea105a2faea01dcc14acb32e52
martin-bobek <martinbobek8@gmail.com>
2019-11-03 00:02:23 -0600

    Reformatted the Tree class declaration.

--- RegexSyntaxTree.h
+++ RegexSyntaxTree.h
@@ -17,13 +17,14 @@ namespace synTree {
     };
 }
 
-class Tree
-{
+class Tree {
 public:
     Tree() = default;
     explicit Tree(const std::string &input);
+
     NFA GenNfa(size_t acceptingType) const { return NFA::Complete(node->GenNfa(), acceptingType); }
     operator bool() const { return (bool)node; }
+
 private:
     std::unique_ptr<synTree::Node> node;
 };
